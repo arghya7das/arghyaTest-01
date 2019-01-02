05ec714fbbf5035c9eb39d7e7fe28eb122744828
arghya <arghya7.das@gmail.com>
2019-01-01 23:12:27 -0500

    First github commit

---
+++ itrtn.py
@@ -0,0 +1,17 @@
+def Main():
+    for step in range(5):
+        print(step)
+
+    words = ['cat','bat','mat','sat','rat']
+    for word in words:
+        print(word)
+    num  = 0
+    while num <= 0:
+        num = int(input("Please enter a positive integer number: "))
+
+
+
+
+
+if __name__ == "__main__":
+    Main()
